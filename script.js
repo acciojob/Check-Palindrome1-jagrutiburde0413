291ba9c6bb9c8eda904b21ab8252b55ac5500adf
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-03-20 20:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function palindrome(string) {
     const len = string.length;
 
     // loop through half of the string
-    for (let i = 0; i <= len / 2; i++) {
+    for (let i = 0; i<len; i++) {
 
         // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
+        if (string[i] !== string[len - i]) {
             return false;
         }
     }
