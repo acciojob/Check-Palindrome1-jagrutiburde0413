4e0258aa9dba272169986f0ebc2ee06c0ef3007e
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-03-20 19:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(string){
+  const len = string.length;
 
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
 }
 module.exports = palindrome
